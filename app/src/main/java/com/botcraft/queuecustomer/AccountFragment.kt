04f49945d903db30f1ce2864c0321a0c379f02ea
vignesh2589 <vigneshp786@gmail.com>
2020-02-10 22:50:13 +0530

    initial_commit_by_vignesh

--- app/src/main/java/com/botcraft/queuecustomer/AccountFragment.kt
+++ app/src/main/java/com/botcraft/queuecustomer/AccountFragment.kt
@@ -3,10 +3,10 @@ package com.botcraft.queuecustomer
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_account.view.*
 
 
@@ -41,6 +41,7 @@ class AccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        //Vignesh
         val view = inflater.inflate(R.layout.fragment_account, container, false)
         val sharedPref: SharedPreferences = activity!!.getSharedPreferences(PREF_NAME, PRIVATE_MODE)
         view.mobile_no.setText(sharedPref.getString("mobile", null))
